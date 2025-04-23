f15fd6ab8043a80befeaccaecd08a7ad5b0562cb
Aayush Sharma <aayushsharma47163@gmail.com>
2025-04-23 14:09:16 +0530

    Fixed an error with sign in

--- ResumeBuilder/src/components/custom/Header.jsx
+++ ResumeBuilder/src/components/custom/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router-dom'
-import { UserButton, useUser } from '@clerk/clerk-react'
+import { UserButton } from '@clerk/clerk-react'
 import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
 
 function Header(){
-    const {user,isSignedIn}=useUser();
     return(
         <div className='p-3 px-5 flex justify-between shadow-md'>
             <Link to={'/dashboard'}>
