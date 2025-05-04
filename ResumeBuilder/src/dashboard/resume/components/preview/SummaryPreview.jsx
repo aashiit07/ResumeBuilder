90f7cef6859d116d1e421bfa416e438a330dbde7
Akg <ayushkrgupta2003@gmail.com>
2025-05-04 15:20:51 +0530

    till experience styling updated

--- ResumeBuilder/src/dashboard/resume/components/preview/SummaryPreview.jsx
+++ ResumeBuilder/src/dashboard/resume/components/preview/SummaryPreview.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 function SummaryPreview({resumeInfo}) {
   return (
-    <p className='text-xs'>
+    <>
+    <h2 className=' font-bold text-sm '
+      style={{
+        color:resumeInfo?.themeColor ||"#ff6666",
+        letterSpacing: "0.5px"
+      }}>
+        SUMMARY
+      </h2>
+      <hr
+        className='border-[1px] my-1'
+        style={{
+          borderColor: resumeInfo?.themeColor || "#ff6666"
+        }}
+      />
+
+      <p className='text-xs'>
         {resumeInfo?.summery}
         </p>
+        
+    </>
+    
 
   )
 }
