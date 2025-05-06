deb5c89a5ba421b56fd0b66ad50141171190253c
Akg <ayushkrgupta2003@gmail.com>
2025-05-06 20:59:38 +0530

    printresume-v1

--- ResumeBuilder/src/dashboard/resume/components/ResumePreview.jsx
+++ ResumeBuilder/src/dashboard/resume/components/ResumePreview.jsx
@@ -7,23 +7,27 @@ import EducationalPreview from './preview/EducationalPreview'
 import SkillsPreview from './preview/SkillsPreview'
 import CertificatePreview from './preview/CertificatePreview'
 import AchievementPreview from './preview/AchievementsPreview'
-function ResumePreview(){
-
-
-    const{resumeInfo,setResumeInfo}=useContext(ResumeInfoContext)
-    return(
-        <div className='flex justify-center'>
-        <div className='shadow-lg h-full px-10 py-5 border w-9/12'>
-           <PersonalDetailPreview resumeInfo={resumeInfo}/>
-           {resumeInfo?.education?.length>0&&   <EducationalPreview resumeInfo={resumeInfo}/>}
-           <SummaryPreview resumeInfo={resumeInfo}/>
-           {resumeInfo?.experience?.length>0&&  <ExperiencePreview resumeInfo={resumeInfo}/>}
-           {resumeInfo?.skills?.length>0&&     <SkillsPreview resumeInfo={resumeInfo}/>}
-           {resumeInfo?.certificates?.length>0&&     <CertificatePreview resumeInfo={resumeInfo}/>}
-           {resumeInfo?.achievements?.length>0&&     <AchievementPreview resumeInfo={resumeInfo}/>}
-           
+function ResumePreview() {
+    const { resumeInfo } = useContext(ResumeInfoContext);
+  
+    return (
+      <div className="flex justify-center bg-white">
+        <div 
+          className="w-full max-w-2xl p-4 border" // Reduced padding, fixed max-width
+          style={{ 
+            width: '210mm', // A4 width in mm
+            minHeight: 'fit-content', // Adjust to content
+          }}
+        >
+          <PersonalDetailPreview resumeInfo={resumeInfo} />
+          {resumeInfo?.education?.length > 0 && <EducationalPreview resumeInfo={resumeInfo} />}
+          <SummaryPreview resumeInfo={resumeInfo} />
+          {resumeInfo?.experience?.length > 0 && <ExperiencePreview resumeInfo={resumeInfo} />}
+          {resumeInfo?.skills?.length > 0 && <SkillsPreview resumeInfo={resumeInfo} />}
+          {resumeInfo?.certificates?.length > 0 && <CertificatePreview resumeInfo={resumeInfo} />}
+          {resumeInfo?.achievements?.length > 0 && <AchievementPreview resumeInfo={resumeInfo} />}
         </div>
-        </div>
-    )
-}
+      </div>
+    );
+  }
 export default ResumePreview
